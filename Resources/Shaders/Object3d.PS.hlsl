372fe46be99a036dbdb421dceeff0ea5e2b89119
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-08-13 13:56:49 +0900

    シェーダー変更

--- Resources/Shaders/Object3d.PS.hlsl
+++ Resources/Shaders/Object3d.PS.hlsl
@@ -1,3 +1,5 @@
+#include"Object3d.hlsli"
+
 struct Material
 {
     float32_t4 color;
@@ -10,7 +12,7 @@ struct PixelShaderOutput
 	float32_t4 color : SV_TARGET0;
 };
 
-PixelShaderOutput main()
+PixelShaderOutput main(VertexShaderOutput input)
 {
 	PixelShaderOutput output;
     output.color = gMaterial.color; //白色
