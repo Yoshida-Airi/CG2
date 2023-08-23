f27093c7881721398a94e5e1f789df9aa5936965
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-08-24 00:04:42 +0900

    a

--- main.cpp
+++ main.cpp
@@ -118,20 +118,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		sprite->Update();
 
 
-#ifdef _DEBUG
-
-		ImGui::Begin("Setting");
-
-		float translateSprite[] = {spriteData.transform.translate.x,spriteData.transform.translate.y,spriteData.transform.translate.z};
-
-		ImGui::SliderFloat3("translete", translateSprite, 0.0f, 1280.0f);
-
-
-
-		ImGui::End();
-
-#endif
-
 
 		imGuiManager->End();
 
